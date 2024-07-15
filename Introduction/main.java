ef1c31e52e79be618525137c9e141ff47dc67b09
Anadi <anadi@mishra.com>
2024-07-15 22:22:05 +0530

    Java main

---
+++ Introduction/main.java
@@ -0,0 +1,9 @@
+package Introduction;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("This is the introduction of Java, and also learning the git control in Intellij ide");
+
+
+    }
+}
