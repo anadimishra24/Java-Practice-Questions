5970e19b57404cef4cffbd7a78d7779ad9099be0
Anadi <anadi@mishra.com>
2024-07-16 15:58:54 +0530

    Array starting

---
+++ Array/Main.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Context of Array in Java\n");
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println(numbers[0]); // Output: 10
+
+    }
+}
